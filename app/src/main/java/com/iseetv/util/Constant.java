eab6bb0145c2ff60f29e0f77b5411899627fb158
fando <fandofast@gmail.com>
2020-06-22 12:51:31 +0700

    Initial commit

---
+++ app/src/main/java/com/iseetv/util/Constant.java
@@ -0,0 +1,24 @@
+package com.iseetv.util;
+
+import java.io.Serializable;
+
+public class Constant implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public static String SERVER_URL = "https://watchfmovies.xyz/tv/";
+    public static final String ALLINDOCHANNEL= "https://fando.xyz/gettv.php?url=https://iptv-org.github.io/iptv/countries/id.m3u";
+    public static final String LATEST_URL = "https://fando.xyz/gettv.php?url=https://iptv-org.github.io/iptv/countries/int.m3u";
+    public static final String BASEURLCHANNEL = "https://fando.xyz/gethome.php?url=https://iptv-org.github.io/iptv/countries/";
+    public static final String URLSPORT="https://fando.xyz/gettv.php?url=https://iptv-org.github.io/iptv/categories/sport.m3u";
+    public static final String SEARCH_URL="https://fando.xyz/getsearch.php?q=";
+    public static final String ARRAY_NAME = "LIVETV";
+    public static final String CATEGORY_NAME = "category_name";
+    public static final String CHANNEL_ID = "id";
+    public static final String CHANNEL_TITLE = "channel_title";
+    public static final String CHANNEL_URL = "channel_url";
+    public static final String CHANNEL_IMAGE = "channel_thumbnail";
+    public static final String CHANNEL_AVG_RATE = "rate_avg";
+}
